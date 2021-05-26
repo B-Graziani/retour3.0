9bc9236e39a3655f3df646b013461da85d9bf746
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-05-26 19:14:14 +0200

    great

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,37 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+  def top
+    @flats = Flat.where('stars >=5')
+  end
+  def show
+    @flat = Flat.find(params[:id])
+  end
+  def new
+    @flat = Flat.new
+  end
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to @flat
+  end
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to @flat
+  end
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :address, :description, :stars)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
  root to: "pages#home"
- resources :flats, only: [:index]
+ resources :flats, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  collection do
+    get :top
+  end
+end
 end
